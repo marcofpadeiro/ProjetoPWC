182cc1047655f94d0bca842d6b919349896e7b63
Tomás Moura <2211866@my.ipleiria.pt>
2022-01-30 00:27:19 +0000

    favoritos a funcionar e detalhes em progresso

--- COINGECKO/js/script.js
+++ COINGECKO/js/script.js
@@ -1,10 +1,19 @@
 'use strict';
 var coins = [];
+var favoritos = [];
+if (JSON.parse(localStorage.getItem("favoritos"))== null){
+    for (var i= 0; i < 100; i++){
+        favoritos[i]=false;
+    }
+}else {
+    favoritos = JSON.parse(localStorage.getItem("favoritos"));
+}
+
+
 $.ajax({
     method: "GET",
     url: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=24h"
 }).done(function(res){
-    
     var i = 0;
     $.each(res, function(index, result){
         coins[i] = {
@@ -15,20 +24,17 @@ $.ajax({
             h24: result.price_change_percentage_24h,
             marketcap: result.market_cap.toLocaleString(),
             volume: result.total_volume.toLocaleString(),
-            fav: true
+            fav: favoritos[i]   
         };
         i++;
     })
+
     mostra_top_4(coins);
     mostra_tabela(100, coins);
-    
 })
-
-
 function mostra_tabela(numero){
     var cloneFila = $('#fila').clone()
     var clone = [];
-    
     $('#tbody').empty();
     for(var i = 0; i < numero; i++){
         clone[i] = cloneFila.clone();
@@ -43,8 +49,8 @@ function mostra_tabela(numero){
         $('#market-cap', clone[i]).text(coins[i].marketcap + '€');
         $('#volume-total', clone[i]).text(coins[i].volume + '€');
         $('#index', clone[i]).val(i);
-        if(coins[i].fav == true){ $('#star', clone[i]).attr('src', "../img/starcolor.png"); }
-        else {$('#star', clone[i]).attr('src', "../img/starnocolor.png");}
+        if(coins[i].fav == true){ $('#favoritos', clone[i]).attr('src', "../img/starcolor.png"); }
+        else {$('#favoritos', clone[i]).attr('src', "../img/starnocolor.png");}
         $('#tbody').append(clone[i]);
     }
 }
@@ -98,4 +104,22 @@ function top_moedas(){
 function redirecionaPaginaDetalhes(id){
     localStorage.setItem('id', id);
     location.href = 'detalhes.html';
+}
+function CliqueStar(id, star){
+    if(coins[id].fav == false){
+        star.attr('src', '../img/starcolor.png');
+        coins[id].fav = true;
+    } else {
+        coins[id].fav = false;
+        star.attr('src', '../img/starnocolor.png');
+    }
+    var array = [];
+    for(var i = 0; i < coins.length; i++){
+        if(coins[i].fav == true){
+            array[i] = true;
+        } else {
+            array[i] = false;
+        }
+    }
+    localStorage.setItem("favoritos", JSON.stringify(array));
 }
--- COINGECKO/js/script_detalhes.js
+++ COINGECKO/js/script_detalhes.js
@@ -1,11 +1,19 @@
 'use strict'
 var id = localStorage.getItem('id');
 var coins = [];
+var favoritos = [];
+if (JSON.parse(localStorage.getItem("favoritos"))== null){
+    for (var i= 0; i < 100; i++){
+        favoritos[i]=false;
+    }
+}else {
+    favoritos = JSON.parse(localStorage.getItem("favoritos"));
+}
 $.ajax({
     method: "GET",
     url: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=24h"
 }).done(function(res){
-    
+
     var i = 0;
     $.each(res, function(index, result){
         coins[i] = {
@@ -16,13 +24,35 @@ $.ajax({
             h24: result.price_change_percentage_24h,
             marketcap: result.market_cap.toLocaleString(),
             volume: result.total_volume.toLocaleString(),
-            fav: true
+            fav: favoritos[i] ,
+            abreviatura: result.symbol.toUpperCase()
         };
         i++;
     })
     Menu_Detalhes();
 })
 function Menu_Detalhes(){
+    if(coins[id].fav == true){ $('#favoritos').attr('src', "../img/starcolor.png"); }
+    else {$('#favoritos').attr('src', "../img/starnocolor.png");}
     $('#logo-detalhes').attr('src', coins[id].logo);
     $('#nome-detalhes').text(coins[id].nome);
+    $('#abreviatura').text(coins[id].abreviatura);
+}
+function CliqueStar(star){
+    if(coins[id].fav == false){
+        star.attr('src', '../img/starcolor.png');
+        coins[id].fav = true;
+    } else {
+        coins[id].fav = false;
+        star.attr('src', '../img/starnocolor.png');
+    }
+    var array = [];
+    for(var i = 0; i < coins.length; i++){
+        if(coins[i].fav == true){
+            array[i] = true;
+        } else {
+            array[i] = false;
+        }
+    }
+    localStorage.setItem("favoritos", JSON.stringify(array));
 }
