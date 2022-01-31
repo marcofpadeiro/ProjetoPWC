21a99c2752c6e8cf25e696816d10585b2c9b0767
MarcoPadeiroIPL <2211868@my.ipleiria.pt>
2022-01-31 04:48:22 +0000

    Versão quase final

--- COINGECKO/js/script.js
+++ COINGECKO/js/script.js
@@ -1,6 +1,15 @@
 'use strict';
 var coins = [];
 var favoritos = [];
+var ordemTabela = [];
+var cloneCaixa = $('.caixas').clone();
+var currency = "eur";
+var numero = 100;
+for(var i = 0; i < 100; i++){
+    ordemTabela[i] = i;
+}
+            
+var cloneFila = $('#fila').clone()
 if (JSON.parse(localStorage.getItem("favoritos"))== null){
     for (var i= 0; i < 100; i++){
         favoritos[i]=false;
@@ -8,69 +17,262 @@ if (JSON.parse(localStorage.getItem("favoritos"))== null){
 }else {
     favoritos = JSON.parse(localStorage.getItem("favoritos"));
 }
+window.onload = function() {
+    requestAPI();
+};
 
-
-$.ajax({
-    method: "GET",
-    url: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=24h"
-}).done(function(res){
-    var i = 0;
-    $.each(res, function(index, result){
-        coins[i] = {
-            id: result.market_cap_rank,
-            nome: result.name,
-            logo: result.image,
-            valor: result.current_price > 1 ? result.current_price.toFixed(2) : result.current_price,
-            h24: result.price_change_percentage_24h,
-            marketcap: result.market_cap.toLocaleString(),
-            volume: result.total_volume.toLocaleString(),
-            fav: favoritos[i]   
-        };
-        i++;
+function changeCurrency(cur){
+    if(cur == "eur"){
+        
+        $('#usd').removeClass("focado");
+        $('#euro').removeClass("nao-focado");
+        $('#usd').addClass("nao-focado");
+        $('#euro').addClass("focado");
+        if(currency != "eur"){
+            currency = "eur";
+        }
+        
+        
+        
+    } else {
+        currency = "usd";
+        $('#euro').removeClass("focado");
+        $('#usd').removeClass("nao-focado");
+        $('#euro').addClass("nao-focado");
+        $('#usd').addClass("focado");
+        if(currency != "usd"){
+            currency = "usd";
+        }
+    }
+    requestAPI();
+}
+function requestAPI(){
+    $.ajax({
+        method: "GET",
+        url: "https://api.coingecko.com/api/v3/coins/markets?vs_currency="+ currency + "&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=24h"
+    }).done(function(res){
+        var i = 0;
+        
+        $.each(res, function(index, result){
+            coins[i] = {
+                id: result.market_cap_rank,
+                nome: result.name,
+                logo: result.image,
+                valor: result.current_price >= 1 ? result.current_price.toFixed(2) : result.current_price,
+                h24: result.price_change_percentage_24h,
+                marketcap: result.market_cap,
+                volume: result.total_volume,
+                fav: favoritos[i]   ,
+                mostrar:true,
+                abreviatura: result.symbol
+            };
+            i++;
+        })
+        mostra_top_4();
+        mostra_tabela();
     })
-    mostra_top_4(coins);
-    mostra_tabela(100, coins);
-})
-function mostra_tabela(numero){
-    var cloneFila = $('#fila').clone()
+}
+
+function OrdenarTabela(item, o){
+    var aux = 0, aux_i = 0;
+    var valor = [], h24 = [], marketCap = [], volume_total = [];
+    switch(o){
+        case '1':
+            // ordem id crescente
+            for(var i = 0; i < 100; i++){
+                ordemTabela[i] = i;
+            }
+            $(item).attr('value', '2');
+            break;
+        case '2': 
+            // ordem id decrescente 
+            for(var i = 0; i < 100; i++){
+                ordemTabela[i] = 99 - i;
+            }
+            $(item).attr('value', '1');
+            break;
+        case '3': // ordem valor crescente
+            aux = 0, aux_i = 0;
+            var valor = [];
+            for(var i = 0; i < coins.length; i++){
+                valor[i] = parseFloat(coins[i].valor);
+                ordemTabela[i] = i;
+            }
+
+            for (var i = 0; i < coins.length; i++) {
+                for (var j = i; j < coins.length; j++) {
+                    if (valor[j] > valor[i]) {
+                        aux = valor[j];
+                        aux_i = ordemTabela[j];
+                        valor[j] = valor[i];
+                        ordemTabela[j] = ordemTabela[i]; 
+                        valor[i] = aux;
+                        ordemTabela[i] = aux_i;
+                    }
+                }
+            }
+            $(item).attr('value', '4');
+            break;
+        case '4': 
+            aux = 0, aux_i = 0;
+            for(var i = 0; i < coins.length; i++){
+                valor[i] = parseFloat(coins[i].valor);
+                ordemTabela[i] = i;
+            }
+            for (var i = 0; i < coins.length; i++) {
+                for (var j = i; j < coins.length; j++) {
+                    if (valor[j] < valor[i]) {
+                        aux  = valor[j];
+                        aux_i = ordemTabela[j];
+                        valor[j] = valor[i];
+                        ordemTabela[j] = ordemTabela[i]; 
+                        valor[i] = aux;
+                        ordemTabela[i] = aux_i;
+                    }
+                }
+            }
+            $(item).attr('value', '3');
+            break;
+        case '5': 
+            aux = 0, aux_i = 0;
+            for(var i = 0; i < coins.length; i++){
+                h24[i] = parseFloat(coins[i].h24);
+                ordemTabela[i] = i;
+            }
+            for (var i = 0; i < coins.length; i++) {
+                for (var j = i; j < coins.length; j++) {
+                    if (h24[j] > h24[i]) {
+                        aux  = h24[j];
+                        aux_i = ordemTabela[j];
+                        h24[j] = h24[i];
+                        ordemTabela[j] = ordemTabela[i]; 
+                        h24[i] = aux;
+                        ordemTabela[i] = aux_i;
+                    }
+                }
+            }
+            $(item).attr('value', '6');
+            break;
+        case '6': 
+            aux = 0, aux_i = 0;
+            for(var i = 0; i < coins.length; i++){
+                h24[i] = parseFloat(coins[i].h24);
+                ordemTabela[i] = i;
+            }
+            for (var i = 0; i < coins.length; i++) {
+                for (var j = i; j < coins.length; j++) {
+                    if (h24[j] < h24[i]) {
+                        aux  = h24[j];
+                        aux_i = ordemTabela[j];
+                        h24[j] = h24[i];
+                        ordemTabela[j] = ordemTabela[i]; 
+                        h24[i] = aux;
+                        ordemTabela[i] = aux_i;
+                    }
+                }
+            }
+            $(item).attr('value', '5');
+            break;
+        case '7': 
+            aux = 0, aux_i = 0;
+            for(var i = 0; i < coins.length; i++){
+                volume_total[i] = parseInt(coins[i].volume);
+                ordemTabela[i] = i;
+            }
+            for (var i = 0; i < coins.length; i++) {
+                for (var j = i; j < coins.length; j++) {
+                    if (volume_total[j] > volume_total[i]) {
+                        aux  = volume_total[j];
+                        aux_i = ordemTabela[j];
+                        volume_total[j] = volume_total[i];
+                        ordemTabela[j] = ordemTabela[i]; 
+                        volume_total[i] = aux;
+                        ordemTabela[i] = aux_i;
+                    }
+                }
+            }
+            $(item).attr('value', '8');
+            break;
+        case '8': 
+            aux = 0, aux_i = 0;
+            for(var i = 0; i < coins.length; i++){
+                volume_total[i] = parseInt(coins[i].volume);
+                ordemTabela[i] = i;
+            }
+            for (var i = 0; i < coins.length; i++) {
+                for (var j = i; j < coins.length; j++) {
+                    if (volume_total[j] < volume_total[i]) {
+                        aux  = volume_total[j];
+                        aux_i = ordemTabela[j];
+                        volume_total[j] = volume_total[i];
+                        ordemTabela[j] = ordemTabela[i]; 
+                        volume_total[i] = aux;
+                        ordemTabela[i] = aux_i;
+                    }
+                }
+            }
+            $(item).attr('value', '7');
+            break;
+    }
+    mostra_tabela(numero);
+}
+function mostra_tabela(){
+    
     var clone = [];
     $('#tbody').empty();
-    for(var i = 0; i < numero; i++){
-        clone[i] = cloneFila.clone();
-        $(clone[i]).val(i);
-        $('#id', clone[i]).text(coins[i].id);
-        $('#nome', clone[i]).text(coins[i].nome);
-        $('#logo-coin', clone[i]).attr('src', coins[i].logo);
-        $('#valor', clone[i]).text(coins[i].valor + '€');
-        var color = coins[i].h24 < 0 ? 'red' : 'rgb(21, 223, 14)';
-        $('#h24', clone[i]).css('color', color);
-        $('#h24', clone[i]).text(coins[i].h24.toFixed(2) + '%');
-        $('#market-cap', clone[i]).text(coins[i].marketcap + '€');
-        $('#volume-total', clone[i]).text(coins[i].volume + '€');
-        $('#index', clone[i]).val(i);
-        if(coins[i].fav == true){ $('#favoritos', clone[i]).attr('src', "../img/starcolor.png"); }
-        else {$('#favoritos', clone[i]).attr('src', "../img/starnocolor.png");}
-        $('#tbody').append(clone[i]);
+    var i = 0;
+    for(i = 0; i < numero; i++){
+        var index = ordemTabela[i];    
+        if(coins[index].mostrar == true){
+            clone[index] = cloneFila.clone();
+            $(clone[index]).val(index);
+            $('#id', clone[index]).text(coins[index].id);
+            $('#nome', clone[index]).text(coins[index].nome);
+            $('#logo-coin', clone[index]).attr('src', coins[index].logo);
+            var sign = currency == "eur" ? "€" : "$";
+            
+            $('#valor', clone[index]).text(coins[index].valor + sign);
+            var color = coins[index].h24 < 0 ? 'red' : 'rgb(21, 223, 14)';
+            $('#h24', clone[index]).css('color', color);
+            $('#h24', clone[index]).text(coins[index].h24.toFixed(2) + '%');
+            $('#market-cap', clone[index]).text(coins[index].marketcap.toLocaleString() + sign);
+            $('#volume-total', clone[index]).text(coins[index].volume.toLocaleString() + sign);
+            $('#index', clone[index]).val(index);
+            if(coins[index].fav == true){ $('#favoritos', clone[index]).attr('src', "../img/starcolor.png"); }
+            else {$('#favoritos', clone[index]).attr('src', "../img/starnocolor.png");}
+            $('#tbody').append(clone[index]);
+        }
     }
-    
+}
+function toggleTop10(){
+    if(numero == 100){
+        numero = 10;
+        $('#showTop10').html("Top 100");
+    } else {
+        numero = 100;
+        $('#showTop10').html("Top 10");
+    }
+    mostra_tabela();
 }
 function mostra_top_4(){
-    var cloneCaixa = $('.caixas').clone();
-    var top_moeda = top_moedas(coins);
+    
+    var top_moeda = top_moedas();
     var clone = [];
     $('.top4').empty(); 
     for(var i = 0; i < 4; i++){
-        clone[i] = cloneCaixa.clone();
-        $('#logo-coin-top', clone[i]).attr('src', top_moeda[i].logo);
-        $('#nome-top', clone[i]).text(top_moeda[i].nome);
-        $('#valor-top', clone[i]).text(top_moeda[i].valor + '€');
-        var color = top_moeda[i].h24 < 0 ? 'red' : 'rgb(6, 167, 54)'; 
-        $('#h24-top', clone[i]).css('color', color);
-        $('#h24-top', clone[i]).text('+' + top_moeda[i].h24.toFixed(2) + '%');
-        $('#ranking-top', clone[i]).text('#' + top_moeda[i].id);
-        $('.top4').append(clone[i]);
+            clone[i] = cloneCaixa.clone();
+            $('#logo-coin-top', clone[i]).attr('src', top_moeda[i].logo);
+            $('#nome-top', clone[i]).text(top_moeda[i].nome);
+            var sign = currency == "eur" ? "€" : "$";
+            $('#valor-top', clone[i]).text(top_moeda[i].valor + sign);
+            var color = top_moeda[i].h24 < 0 ? 'red' : 'rgb(6, 167, 54)'; 
+            $('#h24-top', clone[i]).css('color', color);
+            $('#h24-top', clone[i]).text('+' + top_moeda[i].h24.toFixed(2) + '%');
+            $('#ranking-top', clone[i]).text('#' + top_moeda[i].id);
+            $('.top4').append(clone[i]);
     }
 }
+
 function top_moedas(){
     var top = [];
     top[0] = coins[0];
@@ -126,4 +328,18 @@ function CliqueStar(id, star){
         }
     }
     localStorage.setItem("favoritos", JSON.stringify(array));
+}
+function search_coin() {
+    var input = $('#pesquisa').val().toLowerCase(); // vai buscar input
+
+    for (i = 0; i < coins.length; i++) { 
+        if (coins[i].nome.toLowerCase().includes(input) || coins[i].abreviatura.toLowerCase().includes(input) || coins[i].id + 1 == input) {
+            coins[i].mostrar = true;
+        }
+        else {
+            coins[i].mostrar = false;        
+        }
+        
+    }
+    mostra_tabela(100);
 }
--- COINGECKO/js/script_detalhes.js
+++ COINGECKO/js/script_detalhes.js
@@ -13,40 +13,129 @@ $.ajax({
     method: "GET",
     url: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=24h"
 }).done(function(res){
-
     var i = 0;
     $.each(res, function(index, result){
         coins[i] = {
             id: result.market_cap_rank,
             nome: result.name,
             logo: result.image,
+            name_id:result.id,
             valor: result.current_price > 1 ? result.current_price.toFixed(2) : result.current_price,
             h24: result.price_change_percentage_24h,
             marketcap: result.market_cap.toLocaleString(),
             volume: result.total_volume.toLocaleString(),
             fav: favoritos[i] ,
             abreviatura: result.symbol.toUpperCase()
+
         };
         i++;
     })
     Menu_Detalhes();
+    requestAPI_graph()
+    
+    
 })
+
+function requestAPI_graph(){
+        $.ajax({
+            method: "GET",
+            url: "https://api.coingecko.com/api/v3/coins/"+ coins[id].name_id +"/market_chart?vs_currency=eur&days=90&interval=daily"
+        }).done(function(res){
+                console.log(res.prices);
+                var limit = res.prices.length;    //increase number of dataPoints by increasing the limit
+                var highest = 0;
+                for(var i = 0; i < res.prices.length; i++){
+                    highest = res.prices[0][1] > highest ? res.prices[0][1] : highest;
+                }  
+                var data = [];
+                var dataSeries = { type: "line" };
+                var dataPoints = [];
+                for (var i = 0; i < limit ; i++) {
+                    
+                    dataPoints.push({
+                        x: i,
+                        y: res.prices[i][1]
+                    });
+                }
+                dataSeries.dataPoints = dataPoints;
+                data.push(dataSeries);
+        
+                //Better to construct options first and then pass it as a parameter
+                var options = {
+                    backgroundColor: "#142749",
+                    width:900,
+                    height:280,
+                    
+                    axisX:{
+                        lineColor:  "rgb(102, 167, 189)",
+                        labelFontColor: "rgb(102, 167, 189)",
+                        tickColor: "#142749",
+                        gridColor: "#142749",
+                        interval: 10
+                    },
+                    axisY:{
+                        lineColor:  "rgb(102, 167, 189)",
+                        labelFontColor: "rgb(102, 167, 189)",
+                        tickColor: "#142749",
+                        gridColor: "#142749",
+                        suffix:"€",
+                        
+                    },
+                    
+                    animationEnabled: true,
+                    title: {
+                        fontColor: "rgb(102, 167, 189)",
+                        text: "90 dias"
+                    },
+                    data: [
+                    ],
+                    data
+                };
+        
+                $("#chartContainer").CanvasJSChart(options);
+                $('.canvasjs-chart-credit').html(' ');
+        })
+    }
 function Menu_Detalhes(){
+
     if(coins[id].fav == true){ $('#favoritos').attr('src', "../img/starcolor.png"); }
-    else {$('#favoritos').attr('src', "../img/starnocolor.png");}
+    else {$('#favoritos').attr('src', "../img/starblackwhite.png");}
     $('#logo-detalhes').attr('src', coins[id].logo);
     $('#nome-detalhes').text(coins[id].nome);
     $('#abreviatura').text(coins[id].abreviatura);
-
+    $('#preco-moeda').text(coins[id].valor);
+    var color = coins[id].h24 < 0 ? 'red' : 'rgb(21, 223, 14)';
+    $('#h24-moeda').css('color', color);
+    $('#h24-moeda').text(coins[id].h24.toFixed(2) + '%');
+    $('#valor-moeda').text(coins[id].valor + '€');
+    $('#volume-moeda').text(coins[id].volume + '€');
+    $('#market-moeda').text(coins[id].marketcap + '€');
+    if(coins[id].id == 1){ 
+    $('#rank-moeda').addClass('top1');
+     $('#rank-moeda').removeClass('top2');
+     $('#rank-moeda').removeClass('top3');
+    } else if(coins[id].id == 2){
+        $('#rank-moeda').addClass('top2');
+        $('#rank-moeda').removeClass('top1');
+        $('#rank-moeda').removeClass('top3');
+    } else if(coins[id].id == 3){
+        $('#rank-moeda').addClass('top3');
+        $('#rank-moeda').removeClass('top1');
+        $('#rank-moeda').removeClass('top2');
+    } else {
+        $('#rank-moeda').removeClass('top1');
+        $('#rank-moeda').removeClass('top2');
+        $('#rank-moeda').removeClass('top3');
+    }
+    $('#rank-moeda').text('#' + coins[id].id);
 }
 function CliqueStar(star){
     if(coins[id].fav == false){
-        
         star.attr('src', '../img/starcolor.png');
         coins[id].fav = true;
     } else {
         coins[id].fav = false;
-        star.attr('src', '../img/starnocolor.png');
+        star.attr('src', '../img/starblackwhite.png');
     }
     var array = [];
     for(var i = 0; i < coins.length; i++){
@@ -57,4 +146,16 @@ function CliqueStar(star){
         }
     }
     localStorage.setItem("favoritos", JSON.stringify(array));
+}
+function search_coin() {
+    var input = $('#pesquisa').val(); // vai buscar input
+    for (i = 0; i < coins.length; i++) { 
+        if (coins[i].nome.toLowerCase() == input || coins[i].abreviatura.toLowerCase() == input || coins[i].id == input) {
+            id = coins[i].id -1;
+            Menu_Detalhes();
+            requestAPI_graph()
+        }
+        
+        
+    }
 }
